6f53a4efd2cc350e52867a0757bec319067f4fbe
Mijian Xu <mijian.xu@smail.nju.edu.cn>
2019-10-08 22:12:25 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,16 @@
 
 # hugo --baseUrl="https://xumi1993.github.io/blog"
 cd public
+
+if [ -n "$GITHUB_AUTH_SECRET" ]
+then
+    touch ~/.git-credentials
+    chmod 0600 ~/.git-credentials
+    echo $GITHUB_AUTH_SECRET > ~/.git-credentials
+    git config credential.helper store
+    git config user.email "gomijianxu@gmail.com"
+    git config user.name "xumi1993"
+fi
 git add .
 git commit -m `date +"%Y.%m.%dT%H:%M%S"`
-git push origin master
+git push --force origin HEAD:master
